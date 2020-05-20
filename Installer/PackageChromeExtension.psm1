4c46f3e4ab36ab3332a2ba61e4a86518ece1d689
XavierLucatero <xavier@nearshoredevs.com>
2020-05-20 15:27:52 -0500

    Add module to package the chrome extension

---
+++ Installer/PackageChromeExtension.psm1
@@ -0,0 +1,16 @@
+Import-Module .\Prettify.psm1
+
+function Write-ChromePackage($path, $pemFilePath){    
+    $chrome = Get-ChromeFileName
+    if ($null -eq $pemFilePath ) {
+        Write-HostStep "Packaging chrome extension and pem file"
+        Start-Process -FilePath $chrome --pack-extension=$path
+    }else{
+        Write-HostStep "Packaging chrome extension using an existing pem file"
+        Start-Process -FilePath $chrome --pack-extension=$path --pack-extension-key=$pemFilePath
+    }
+}
+
+function Get-ChromeFileName(){
+    (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)'
+}
