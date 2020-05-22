bf5f82fc56ba0ad8029f1d10751ecba6279e3656
XavierLucatero <xavier@nearshoredevs.com>
2020-05-22 17:30:44 -0500

    Install Chrome extension
    * Check if MSSQL exist before installing
    * Install nuget needed by MSSQL if it doesn't exists
    * Install chrome if it doesn't exits
    * Install chrome extension

--- Installer/InstallSteps.psm1
+++ Installer/InstallSteps.psm1
@@ -3,13 +3,51 @@ Import-Module "$PSScriptRoot\IIS" -Force #-Verbose #-Force
 Import-Module "$PSScriptRoot\Prettify" -Force #-Verbose #-Force
 Import-Module "$PSScriptRoot\ImportStudentInformation" -Force -DisableNameChecking #-Verbose #-Force
 
-Function Install-Chocolatey(){
-    if(!(Test-Path "$($env:ProgramData)\chocolatey\choco.exe"))
-    {
+Function Install-Chocolatey() {
+    if (!(Test-Path "$($env:ProgramData)\chocolatey\choco.exe")) {
         Write-Host "Installing: Cocholatey..."
         Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    }else{Write-Host "Skipping: Cocholatey is already installed."}
+    }else { Write-Host "Skipping: Cocholatey is already installed." }
 }
+
+Function Get-GoogleChromeExePath() {
+    return (Get-ItemProperty -ErrorAction Ignore  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)'
+}
+
+
+Function Install-GoogleChrome() {
+    $exePath = Get-GoogleChromeExePath
+    if ( $exePath.Length -eq 0 ) {
+    #$installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*)
+    #if( $null -eq ($installed | Where-Object { $_.DisplayName -eq "Google Chrome" }) ){    
+        Write-Host
+        Write-Host "Installing Google Chrome web browser"
+        choco install googlechrome -y 
+    }
+}
+Function Add-GoogleExtensionToExtensionInstallForcelist($extensionId) {
+    $regLocation = 'Software\Policies\Google\Chrome\ExtensionInstallForcelist'
+    If (!(Test-Path "HKCU:\$regLocation")) {
+        [int]$count = 0
+        New-Item -Path "HKCU:\$regLocation" -Force
+    }
+    Else {
+        if( ((Get-ItemProperty "HKCU:\$regLocation").PSObject.Properties | Where-Object { $_.Value -like 'bfnmmobhknbpakaapfddhbpofiidofdg;*' }).Count -gt 0){
+            return
+        }
+        [int]$count = (((Get-ItemProperty "HKCU:\$regLocation").PSObject.Properties | 
+            #Check if the property name is an int
+            Where-Object { $_.Name -match "^[\d\.]+$" }) | 
+            #Convert property name to int
+            ForEach-Object -Process { [int]$_.Name } | 
+            #Get Maximum
+            Measure-Object -Maximum).Maximum
+    }
+    $regName = $count + 1
+    $regValue = "$extensionId;https://clients2.google.com/service/update2/crx"
+    $null = New-ItemProperty -Path "HKCU:\$regLocation" -Name $regName -Value $regValue -PropertyType STRING -Force
+}
+
 Function Install-Prerequisites() {
     $allPreReqsInstalled = $true
     
@@ -18,33 +56,37 @@ Function Install-Prerequisites() {
     # Ensure the following are installed.
     Install-Chocolatey
     Install-IISPrerequisites
+    Install-GoogleChrome
     
     #install db libraries
     choco install mysql-connector -y
     # TODO: Ensure that MSSQL module dont exists before installing
-    write-host "Adding powershell SQLServer module"
-    Install-Module -Name SqlServer -Force
+    if ( $null -eq (Get-InstalledModule -Name "SQLServer" -ErrorAction Ignore)) {         
+        Write-Host "Adding powershell SQLServer module"
+        $null = Install-PackageProvider -Name NuGet -Force
+        Install-Module -Name SqlServer -Force
+    }
 
     # MsSQL Server
     #if (!(Test-Path 'HKLM:\Software\Microsoft\Microsoft SQL Server\Instance Names\SQL')) { $allPreReqsInstalled = $false; Write-Host "     Prerequisite not installed: MsSQL-Server" }
 
     # If not all Pre Reqs installed halt!
-    if(!$allPreReqsInstalled){ Write-Error "Error: Missing Prerequisites. Look above for list." -ErrorAction Stop }
+    if (!$allPreReqsInstalled) { Write-Error "Error: Missing Prerequisites. Look above for list." -ErrorAction Stop }
 }
 
 Function Set-AppSettingsJsonFile($jsonFilePath, $config) {
     $appSettings = Get-Content $jsonFilePath -raw | ConvertFrom-Json
 
-    $appSettings.update | % {$appSettings.ConnectionStrings.DefaultConnection = $config.BinaryMetadata.ApiBinaries.ConnectionString.StudentLearningEventsConnectionString}
-    $appSettings.update | % {$appSettings.encryptionExportedKey = $config.EncryptionKey}
+    $appSettings.update | % { $appSettings.ConnectionStrings.DefaultConnection = $config.BinaryMetadata.ApiBinaries.ConnectionString.StudentLearningEventsConnectionString }
+    $appSettings.update | % { $appSettings.encryptionExportedKey = $config.EncryptionKey }
     
-    $appSettings | ConvertTo-Json -depth 32| set-content $jsonFilePath
+    $appSettings | ConvertTo-Json -depth 32 | set-content $jsonFilePath
 }
 
-Function Install-Binaries($settings, $tempPathForBinaries){
+Function Install-Binaries($settings, $tempPathForBinaries) {
     $filePath = $tempPathForBinaries + "\" + $settings.Name + ".zip"
     # Optimization (Caching Packages): Check to see if file exists. If it does NOT then download.
-    if(!(Test-Path $filePath -PathType Leaf)) {
+    if (!(Test-Path $filePath -PathType Leaf)) {
         Write-Host "     Downloading " $settings.BinaryUrl " to -> " $filePath
         Invoke-WebRequest -Uri $settings.BinaryUrl -OutFile $filePath
     }
@@ -54,6 +96,7 @@ Function Install-Binaries($settings, $tempPathForBinaries){
     Expand-Archive -LiteralPath $filePath -DestinationPath $installPath -Force
 
 }
+
 
 # All Install Options go here
 Function Install-StudentEngagementTracker() {
@@ -105,7 +148,12 @@ Function Install-StudentEngagementTracker() {
     if ($destConnStr.Trim().Length -gt 0 -and $sourceConnStr.Trim().Length -gt 0) {
         Import-StudentInfo $sourceConnStr $destConnStr
     }
+
+    #6) Install extension
+    Write-HostStep "Step: Adding Chrome extension "
+    Add-GoogleExtensionToExtensionInstallForcelist "bfnmmobhknbpakaapfddhbpofiidofdg"
  
     Write-HostStep "Step: Opening webapi"
-    Start-Process "https://localhost/StudentEngagement/api/LearningActivityEvents"
+    $chromeExe = Get-GoogleChromeExePath
+    Start-Process $chromeExe  "https://localhost/StudentEngagement/api/LearningActivityEvents"
 }
